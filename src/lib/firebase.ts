c99006e038e79b0188fe019120dc8f661fcfbdd1
Gonzalo Mellao Manriquez <Gonzalo.Mellao.M@gmail.com>
2025-08-17 22:29:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   projectId: "shiftflow-b9vy2",
@@ -15,6 +14,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { app, db, auth };
+export { app, db };
